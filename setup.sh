730753ef2d2a32ac7a876b79d61046e6282546dc
Marcos T Munhoz <3648104+marcostmunhoz@users.noreply.github.com>
2024-09-30 00:14:40 -0300

    feat: initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+source .env
+
+helm install fastfood --set monolith.db.host=$DB_HOST --set monolith.db.username=$DB_USERNAME --set monolith.db.password=$DB_PASSWORD --set monolith.db.database=$DB_DATABASE --set monolith.jwt.secret=$JWT_SECRET $CHART_PATH
